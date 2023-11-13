ef71d3a95831541f2f032f7ec6bd9d7948d5fd18
AND1993 <andre_fonsecasantos@hotmail.com>
2023-11-13 11:38:46 -0300

    Create button.js
    Botão dos votos dos candidatos

---
+++ button.js
@@ -0,0 +1,7 @@
+            function alterarVotos(valor) {
+                document.getElementById("votos").innerHTML = valor;
+            }
+    
+            function alterarano(ano) {
+                document.getElementById("ano").innerHTML = ano;
+            }
